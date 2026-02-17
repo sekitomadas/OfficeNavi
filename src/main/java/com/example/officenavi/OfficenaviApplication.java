144cfd49270607a208e142756394b9dc909161c4
sekitomodas <s.tomoya0319@icloud.com>
2026-02-17 17:49:18 +0900

    initial commit

---
+++ src/main/java/com/example/officenavi/OfficenaviApplication.java
@@ -0,0 +1,13 @@
+package com.example.officenavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficenaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficenaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/officenavi/OfficenaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.officenavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficenaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
